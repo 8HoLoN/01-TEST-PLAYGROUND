70a9ed0cde4fce7db7978943ad6f9f855cf5fb7f
8HoLoN <8holon@gmail.com>
2019-08-25 16:49:35 +0200

    Update optional-chaining.js

--- docs/optional-chaining.js
+++ docs/optional-chaining.js
@@ -1,4 +1,4 @@
-// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
 export default () => {
     const a = {d:{e:'f'};
     return a.b?.c===undefined && a.d?.e==='f';
