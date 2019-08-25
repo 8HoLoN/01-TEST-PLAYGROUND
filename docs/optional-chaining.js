4fa26d847190f42e10a48134630b8950fe682b23
8HoLoN <8holon@gmail.com>
2019-08-25 16:23:37 +0200

    Update and rename test.js to optional-chaining.js

---
+++ docs/optional-chaining.js
@@ -0,0 +1,5 @@
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+export default () => {
+    const a = {d:{e:'f'};
+    return a.b?.c===undefined && a.d?.e==='f';
+};
