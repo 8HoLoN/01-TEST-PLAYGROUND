a26cf0f178179d647a751b00ab4ac806229055ad
8HoLoN <8holon@gmail.com>
2019-08-28 19:37:18 +0200

    Update bigint.js

--- docs/bigint.js
+++ docs/bigint.js
@@ -1,2 +1,4 @@
-
-typeof 10n === 'bigint'
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
+export default () => {
+    return typeof 10n === 'bigint';
+};
