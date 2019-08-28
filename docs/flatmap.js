0de65c02f5f7d9639c48d3432a2ac412fb7e7caa
8HoLoN <8holon@gmail.com>
2019-08-28 19:58:17 +0200

    Update flatmap.js

--- docs/flatmap.js
+++ docs/flatmap.js
@@ -1,4 +1,4 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
 export default () => {
-    return typeof Array.flatMap === 'function';
+    return typeof Array.prototype.flatMap === 'function';
 };
