9bdad52a9b7b999e821cd20491b10dc7421e0c16
8HoLoN <8holon@gmail.com>
2019-08-28 19:57:50 +0200

    Update flat.js

--- docs/flat.js
+++ docs/flat.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
 export default () => {
-    return typeof Array.flat === 'function';
+    return typeof Array.prototype.flat === 'function';
 };
 
